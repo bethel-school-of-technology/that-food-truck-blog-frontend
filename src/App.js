06e62c5d2538bada637fe520be508ee951aede22
Ethan Ferrier <Fethanerrier@gmail.com>
2020-07-18 13:46:38 -0700

    Added basic Bootstrap NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
+import Header from './components/Header'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
