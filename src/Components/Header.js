b143727141a77675ec66a6f220f2d46f8ad17cdd
Aubrena T <aubrenataylor@gmail.com>
2020-07-23 23:41:29 -0400

    added dropdown to navbar

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import './Header.css';
-import logo from './thatFoodTruck.png';
+import logo from './foodTruck2.png';
 
 class Header extends Component {
     render() { 
         return ( 
-          <nav className="navbar navbar-expand-lg navbar-light bg-light border-0">
+          <nav className="navbar navbar-expand-lg navbar-light bg-transparent border-0">
           <img className="img-fluid" src={logo} alt="logo" />
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -19,10 +19,25 @@ class Header extends Component {
               <li className="nav-item active">
                 <a className="nav-link" href="/about">ABOUT</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="/Blog List">BLOG</a>
+              <li className="nav-item">
+                <a className="nav-link active" href="/Blog List">BLOG</a>
               </li>
-            </ul>
+              <li className="nav-item">
+                <a className="nav-link active" href="/Contact">CONTACT</a>
+              </li>
+              <div className="dropdown">
+              <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                SEASONAL MENUS
+              </button>
+              <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a className="dropdown-item" href="#">FALL</a>
+                <a className="dropdown-item" href="#">WINTER</a>
+                <a className="dropdown-item" href="#">SPRING</a>
+                <a className="dropdown-item" href="#">SUMMER</a>
+              </div>
+            </div>
+            </ul>            
+
           </div>
         </nav>
 
