95d2a35ad908f028448d762d595e2f1046307850
Ethan Ferrier <Fethanerrier@gmail.com>
2020-07-25 12:35:53 -0700

    Updated Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,22 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faFacebook,
+    faTwitter,
+    faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 
 const Footer = () => {
     return(
-        <div className="page-footer">
+        <div className="page-footer mt-3">
             <div className="container">
                 <div className="row">
                     {/* column 1 */}
                     <div className="col-md-3 col-sm-6">
                         <h4> Contact Info  </h4>
                         <ul className="list-unstyled">
-                            <li> The Food Truck Blog </li>
+                            <li> The Food Truck Blog Inc. </li>
                             <li> 2244 Golden Sunset Dr.</li>
                             <li> 530-222-9448 </li>
                             <li> foodtruckblogs@gmail.com </li>
@@ -19,12 +24,25 @@ const Footer = () => {
                     </div>
                      {/* column 2 */}
                      <div className="col-md-3 col-sm-6">
-                        <h4> Social Media </h4>
+                        <h4> Social Media Links </h4>
                         <ul className="list-unstyled">
-                            <li> </li>
-                            <li>  </li>
-                            <li> </li>
-                            <li>  </li>
+                            <li> @FoodTruckBlog</li>
+                           
+                                <a
+                                href="https://www.facebook.com"
+                                className="facebook social" >
+                                <FontAwesomeIcon icon = {faFacebook} size= "2x"/> </a>
+                                   
+                                <a
+                                href="https://www.twitter.com"
+                                className="twitter social" >
+                                <FontAwesomeIcon icon = {faTwitter} size= "2x"/> </a>
+                                   
+                                <a
+                                href="https://www.Instagram.com"
+                                className="instagram social" >
+                                <FontAwesomeIcon icon = {faInstagram} size= "2x"/> </a>
+                                   
                         </ul>
                     </div>
                     
