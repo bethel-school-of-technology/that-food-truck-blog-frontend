1cd3758945503ec3d9a269e863672bf9ab8b5114
Val <valmbonilla@gmail.com>
2020-07-31 15:00:02 -0700

    more styling

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -10,8 +10,8 @@ import Mitch from '../../pictures/mitch.png';
 
 const About = props => {
   return <div className="About">
-     <h4 className="Mission text-center"> Our Mission </h4>
-                    <p> Our hearts are in sharing and expanding the Gospel of Jesus through our Food Truck Businesses. Our goal is that all know Jesus would encounter the love of God through our food, and those that don't know the Fathers love would encounter His love for them. 
+     <h2 className="Mission text-center"> Our Mission </h2>
+                    <p className="card-body mb-2"> Our hearts are in sharing and expanding the Gospel of Jesus through our Food Truck Businesses. Our goal is that all know Jesus would encounter the love of God through our food, and those that don't know the Fathers love would encounter His love for them. 
                         Every meal is made in excellence knowing we want our customers to taste the goodness of God in every bite. 
                     </p>
                     <img className="rounded mx-auto d-block" width="550" height="400" src={Image3} alt= "food truck" /> 
@@ -21,7 +21,7 @@ const About = props => {
                     </p>
                     
                     <div className="container text-center"> 
-                    <h3> Locations </h3>
+                    <h2> Locations </h2>
                     <div className="row">
                     <ul className="container text-center list-group">
                     <li className="list-group-item"> Redding, California </li>
@@ -32,28 +32,6 @@ const About = props => {
                     <li className="list-group-item"> Santa Barbara, California </li>
                     </ul> </div>
                     </div>
-                    
-                    
-                    {/* <p>
-                    <h3 className="Veam text-center"> Our VEAM Founders </h3>
-                    <u> 
-                    <li className="text-center"> Val Bonilla  
-                    <img width="200" height="200" src={Val} alt="Val" /> 
-                      </li>
-                      <li className="text-center">Ethan Ferrier 
-                        <img width="220" height="200" src={Ethan} alt="Ethan"/>
-                        </li> 
-                        <li className="text-center"> Aubrena Taylor
-                          <img width="220" height="200" src={Aubrena} alt="Aubrena"/>
-                        </li>
-                        <li className="text-center"> Mitch Mccuen 
-                          <img width="220" height="200" src={Mitch} alt="Mitch"/>
-                        </li>
-                      </u>
-                    
-                  
-                    </p> */}
-
   </div>;
 };
 
