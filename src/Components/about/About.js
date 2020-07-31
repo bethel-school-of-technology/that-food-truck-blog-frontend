ee8e772b4fc12a5affac13322ad07704e7b30f88
Val <valmbonilla@gmail.com>
2020-07-31 14:39:54 -0700

    Styled About Page

--- src/Components/about/About.js
+++ src/Components/about/About.js
@@ -14,41 +14,45 @@ const About = props => {
                     <p> Our hearts are in sharing and expanding the Gospel of Jesus through our Food Truck Businesses. Our goal is that all know Jesus would encounter the love of God through our food, and those that don't know the Fathers love would encounter His love for them. 
                         Every meal is made in excellence knowing we want our customers to taste the goodness of God in every bite. 
                     </p>
-                    <img width="550" height="400" src={Image3} alt= "food truck" className="center" /> 
-                    <p  className="Location text-left"> <h3> Our Values </h3>
+                    <img className="rounded mx-auto d-block" width="550" height="400" src={Image3} alt= "food truck" /> 
+                    <p  className="container text-center"> <h2> Our Values </h2>
                      <h4> Excellence </h4> Our food is farm to table and 100% organic. <h4> Community </h4> We gather people to join together and feast.
                         <h4> Creativity & Growth  </h4> We are always looking for different ways to expand our menu from glory to glory.
                     </p>
                     
-                    <div className="Location text-right"> 
+                    <div className="container text-center"> 
                     <h3> Locations </h3>
-                    <ul className="text-right">
-                    <li className="text-right"> Redding, California </li>
-                    <li className="text-right"> Atlanta, Georgia </li>
-                    <li> Austin, Texas  </li> 
-                    <li> San Francisco, California </li> 
-                    <li> San Diego, California </li>
-                    <li> Santa Barbara, California </li>
+                    <div className="row">
+                    <ul className="container text-center list-group">
+                    <li className="list-group-item"> Redding, California </li>
+                    <li className="list-group-item"> Atlanta, Georgia </li>
+                    <li className="list-group-item"> Austin, Texas  </li> 
+                    <li className="list-group-item"> San Francisco, California </li> 
+                    <li className="list-group-item"> San Diego, California </li>
+                    <li className="list-group-item"> Santa Barbara, California </li>
                     </ul> </div>
-                    <p>
+                    </div>
+                    
+                    
+                    {/* <p>
                     <h3 className="Veam text-center"> Our VEAM Founders </h3>
                     <u> 
-                    <li> Val Bonilla  
+                    <li className="text-center"> Val Bonilla  
                     <img width="200" height="200" src={Val} alt="Val" /> 
                       </li>
-                      <li>Ethan Ferrier 
-                        <img width="200" height="200" src={Ethan} alt="Ethan"/>
+                      <li className="text-center">Ethan Ferrier 
+                        <img width="220" height="200" src={Ethan} alt="Ethan"/>
                         </li> 
-                        <li> Aubrena Taylor
-                          <img width="200" height="200" src={Aubrena} alt="Aubrena"/>
+                        <li className="text-center"> Aubrena Taylor
+                          <img width="220" height="200" src={Aubrena} alt="Aubrena"/>
                         </li>
-                        <li> Mitch Mccuen 
-                          <img width="200" height="200" src={Mitch} alt="Mitch"/>
+                        <li className="text-center"> Mitch Mccuen 
+                          <img width="220" height="200" src={Mitch} alt="Mitch"/>
                         </li>
                       </u>
                     
                   
-                    </p>
+                    </p> */}
 
   </div>;
 };
