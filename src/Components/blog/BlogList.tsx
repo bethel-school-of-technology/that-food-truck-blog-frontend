810b2ac2c19a145f0d44bee71fbc1c0e24ed2ab5
Ethan Ferrier <Fethanerrier@gmail.com>
2020-08-01 09:59:42 -0700

    BlogList now dynamicly loads data from the server

--- src/Components/blog/BlogList.tsx
+++ src/Components/blog/BlogList.tsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+interface Cards {
+  blogList: Array<Blog>
+}
+
+interface Blog {
+  title: string;
+  text: string;
+  _id: string;
+  date: string;
+  name: string;
+  user: string;
+
+}
 
 
 class BlogList extends Component {
-  state = {
+  state: Cards = {
     blogList: []
   };
 
@@ -27,6 +42,9 @@ class BlogList extends Component {
 
 
   render() {
+    const { blogList = [] } = this.state;
+
+
     return (
       <div className='container mb-3'>
         <div className='card text-center mt-3'>
@@ -39,24 +57,24 @@ class BlogList extends Component {
           </div>
           <div className="card-body">
             <ul className='list-group list-group-flush'>
-              {Object.keys(this.state.blogList).map((blog, index) => {
+              {blogList.map((blog, index) => {
+                let url = "/BlogList/" + blog._id.toString();
                 return (
-                  <div>{blog}</div>
-                  // <li className='list-group-item' key={blog.id}>
-                  //   <div className='card'>
-                  //     <div className='card-blogId'>
-                  //       <h5 className='card-title'>{blog.title}</h5>
-                  //       <h6 className='card-subtitle mb-2 text-muted'>
-                  //         {blog.date}
-                  //       </h6>
-                  //       <p className='card-text'>{blog.text}</p>
-                  //     </div>
-                  //   </div>
-                  //   <a
-                  //     href={blog.id}
-                  //     className='card-link stretched-link'
-                  //   ></a>
-                  // </li>
+                  <li className='list-group-item' key={blog._id}>
+                    <div className='card'>
+                      <div className='card-blogId'>
+                        <h5 className='card-title'>{blog.title}</h5>
+                        <h6 className='card-subtitle mb-2 text-muted'>
+
+                        </h6>
+                        <p className='card-text'>{blog.text}</p>
+                      </div>
+                    </div>
+                    <Link
+                      to={url}
+                      className='card-link stretched-link'
+                    ></Link>
+                  </li>
                 );
               })}
             </ul>
