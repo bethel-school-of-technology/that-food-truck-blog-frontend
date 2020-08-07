ea690c6de2c30a8cf489fb1b48893fc2ca74bf78
Aubrena T <aubrenataylor@gmail.com>
2020-08-07 10:41:45 -0400

    starting update component

--- src/Components/auth/Login.js
+++ src/Components/auth/Login.js
@@ -1,22 +1,19 @@
 import React, { Fragment, useState } from 'react';
-import { Redirect, useHistory } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import './Login.css';
 import axios from 'axios';
-
 const Login = () => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
   });
-
+  const history = useHistory();
   const { email, password } = formData;
-
   const onChange = e =>
     setFormData({
       ...formData,
       [e.target.name]: e.target.value,
     });
-
   const onSubmit = async e => {
     e.preventDefault();
     try {
@@ -35,26 +32,23 @@ const Login = () => {
         body,
         config
       );
-
       const token = res.data;
       localStorage.setItem('jwtToken', JSON.stringify(token));
       localStorage.getItem('jwtToken');
-      // <Redirect to='/home' />;
     } catch (error) {
       return alert('Invalid Credentials');
     }
   };
-
   return (
     <Fragment>
       <div className='container col-9 col-md-5 mb-3'>
-        <div class='card row justify-content-center'>
-          <div class='card-header'>
+        <div className='card row justify-content-center'>
+          <div className='card-header'>
             <div className='h3 card-title'>Admin Sign In</div>
-            <dive class='h6 card-subtitle mb-2 text-muted'>
+            <div className='h6 card-subtitle mb-2 text-muted'>
               for our amazing team only, thank you.
-              <p>username: admin1 password: 123456</p>
-            </dive>
+              <p>username: Admin1 password: 123456</p>
+            </div>
           </div>
           <div className='card-body'>
             <form onSubmit={e => onSubmit(e)}>
@@ -78,7 +72,6 @@ const Login = () => {
                   required
                 />
               </div>
-
               <div className='form-group'>
                 <label for='inputPassword'>Password</label>
                 <input
@@ -93,12 +86,15 @@ const Login = () => {
                 />
               </div>
               <div className='row'>
-                <button className='ml-3 btn-lg  btn-primary ' type='submit'>
+                <button
+                  className='ml-3 btn-lg  btn-primary '
+                  type='submit'
+                  onClick={() => {
+                    history.push('/');
+                  }}
+                >
                   Sign In
                 </button>
-                <p className='col-8 mb-3 text-muted text-right'>
-                  &copy; 2017-2020
-                </p>
               </div>
             </form>
           </div>
@@ -107,13 +103,4 @@ const Login = () => {
     </Fragment>
   );
 };
-
-// Login.propTypes = {
-//   //proptypes
-// };
-
-// const mapStateToProps = state => ({
-//   //
-// });
-
 export default Login;
---
+++ src/Components/menu/SpringMenu.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import './menu.css'
+import falafelWrap from './FalafelWrap.jpg';
+import chxTacos from './grilledChxTacos.jpg';
+import kaleSalad from './kaleSalad.jpg'
+import lambGyro from './Lamb-Gyro.jpg';
+
+const SpringMenu = () => {
+    return(
+
+<div className='menu-page'>
+  `<div id="demo" className="carousel slide" data-ride="carousel">
+
+
+<ul className="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+</ul>
+
+
+<div className="carousel-inner">
+  <div className="carousel-item active">
+    <img src={falafelWrap} alt="Falafel Wrap" />
+  </div>
+  <div className="carousel-item">
+    <img src={chxTacos} alt="Chicken Tacos" />
+  </div>
+  <div className="carousel-item">
+    <img src={kaleSalad} alt="Kale Sald" />
+  </div>
+  <div className="carousel-item">
+  <img src={lambGyro} alt="Lamb Gyro" />
+  </div>
+</div>
+
+
+<a className="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a className="carousel-control-next" href="#demo" data-slide="next">
+  <span className="carousel-control-next-icon"></span>
+</a>
+
+</div>
+
+        </div>
+
+
+
+    );
+};
+
+export default SpringMenu
