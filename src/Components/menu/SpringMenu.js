5a025ed7fa5b00b327933fcf46362bf68d437bd8
Ethan Ferrier <Fethanerrier@gmail.com>
2020-08-08 10:24:09 -0700

    moved images to pictures folder

--- src/Components/menu/SpringMenu.js
+++ src/Components/menu/SpringMenu.js
@@ -1,39 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './menu.css'
-import falafelWrap from './FalafelWrap.jpg';
-import chxTacos from './grilledChxTacos.jpg';
-import kaleSalad from './kaleSalad.jpg'
-import lambGyro from './Lamb-Gyro.jpg';
+import falafelWrap from '../../pictures/FalafelWrap.jpg';
+import chxTacos from '../../pictures/grilledChxTacos.jpg';
+import kaleSalad from '../../pictures//kaleSalad.jpg'
+import lambGyro from '../../pictures/Lamb-Gyro.jpg';
 
 const SpringMenu = () => {
-    return(
-      <div className="container">
-        <h1 className="text-center">SPRING MENU</h1>
+  return (
+    <div className="container">
+      <h1 className="text-center">SPRING MENU</h1>
 
-        <div clasName="menu">
-          <ul className="menu-list text-center w-100">
-            <li className="menu-item row">Falafel Wrap..........$10</li>
-            <li className="menu-item row">Chicken Tacos..........$8</li>
-            <li className="menu-item row">Kale Salad..........$10</li>
-            <li className="menu-item row">Lamb Gyro..........$11</li>
-          </ul>
-          </div>
-        <div className="menu-pictures w-100">
-          <ul className="img-list row w-100">
-            <li className="menu-item-img col"><img className="food-image -w" src={falafelWrap} alt="Falafel Wrap" /></li>
-            <li className="menu-item-img col"><img className="food-image -w" src={chxTacos} alt="Chicken Tacos" /></li>
-            <li className="menu-item-img col"><img className="food-image -w" src={kaleSalad} alt="Kale Salad" /></li>
-            <li className="menu-item-img col"><img className="food-image -w" src={lambGyro} alt="Lamb Gyro" /></li>
+      <div clasName="menu">
+        <ul className="menu-list text-center w-100">
+          <li className="menu-item row">Falafel Wrap..........$10</li>
+          <li className="menu-item row">Chicken Tacos..........$8</li>
+          <li className="menu-item row">Kale Salad..........$10</li>
+          <li className="menu-item row">Lamb Gyro..........$11</li>
+        </ul>
+      </div>
+      <div className="menu-pictures w-100">
+        <ul className="img-list row w-100">
+          <li className="menu-item-img col"><img className="food-image -w" src={falafelWrap} alt="Falafel Wrap" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={chxTacos} alt="Chicken Tacos" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={kaleSalad} alt="Kale Salad" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={lambGyro} alt="Lamb Gyro" /></li>
 
-          </ul>
-        </div>
-        </div>
-      
-  
+        </ul>
+      </div>
+    </div>
 
 
 
-    );
+
+
+  );
 };
 
 export default SpringMenu
--- src/Components/menu/SummerMenu.js
+++ src/Components/menu/SummerMenu.js
@@ -1,39 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './menu.css'
-import burger from './classicburger.jpg';
-import friedChicken from './friedChxSand.jpg';
-import healthyBowl from './healthyBowl.jpg';
-import smoothie from './smoothie.jpg';
+import burger from '../../pictures/classicburger.jpg';
+import friedChicken from '../../pictures/friedChxSand.jpg';
+import healthyBowl from '../../pictures/healthyBowl.jpg';
+import smoothie from '../../pictures/smoothie.jpg';
 
 const SummerMenu = () => {
-    return(
-      <div className="container">
-        <h1 className="text-center">SUMMER MENU</h1>
+  return (
+    <div className="container">
+      <h1 className="text-center">SUMMER MENU</h1>
 
-        <div clasName="menu">
-          <ul className="menu-list text-center w-100">
-            <li className="menu-item row">Classic Buger..........$10</li>
-            <li className="menu-item row">Crispy Chicken Sandwitch..........$12</li>
-            <li className="menu-item row">Quinoa Vegetable Bowl..........$9</li>
-            <li className="menu-item row">Strawberry Banana Smoothie..........$6</li>
-          </ul>
-          </div>
-        <div className="menu-pictures">
-          <ul className="img-list row">
-            <li className="menu-item-img col"><img className="food-image -w" src={burger} alt="Classic Burger" /></li>
-            <li className="menu-item-img col"><img className="food-image -w" src={friedChicken} alt="Fried Chicken Sandwitch" /></li>
-            <li className="menu-item-img col"><img className="food-image -w" src={healthyBowl} alt="Veggie Bowl" /></li>
-            <li className="menu-item-img col"><img className="food-image -w" src={smoothie} alt="Smoothie" /></li>
+      <div clasName="menu">
+        <ul className="menu-list text-center w-100">
+          <li className="menu-item row">Classic Buger..........$10</li>
+          <li className="menu-item row">Crispy Chicken Sandwitch..........$12</li>
+          <li className="menu-item row">Quinoa Vegetable Bowl..........$9</li>
+          <li className="menu-item row">Strawberry Banana Smoothie..........$6</li>
+        </ul>
+      </div>
+      <div className="menu-pictures">
+        <ul className="img-list row">
+          <li className="menu-item-img col"><img className="food-image -w" src={burger} alt="Classic Burger" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={friedChicken} alt="Fried Chicken Sandwitch" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={healthyBowl} alt="Veggie Bowl" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={smoothie} alt="Smoothie" /></li>
 
-          </ul>
-        </div>
-        </div>
-      
-  
+        </ul>
+      </div>
+    </div>
 
 
 
-    );
+
+
+  );
 };
 
 export default SummerMenu
