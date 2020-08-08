f2bf3d1fb85e5f794c5292a53f135965a9bc5033
Ethan Ferrier <Fethanerrier@gmail.com>
2020-08-08 12:18:40 -0700

    fixed shadow

--- src/Components/blog/BlogPage.js
+++ src/Components/blog/BlogPage.js
@@ -50,7 +50,7 @@ const BlogPage = () => {
     <div className='container mb-3'>
       <div class="card row bg-dark text-center text-white col-10 justify-content-center">
         <img class="card-img" src={images[index]} alt="Card image" style={{ height: 500 }}></img>
-        <div class="card-img-overlay shadow" >
+        <div class="card-img-overlay blogShadow" >
           <h5 class="card-title">{title}</h5>
           <p class="card-text">{date}</p>
           <p class="card-text">{text}</p>
