50dd22ba06ae0d017334beeade6bd5df74c88ee5
Aubrena T <aubrenataylor@gmail.com>
2020-08-07 23:16:22 -0400

    finished spring menu need to style

--- src/Components/menu/SpringMenu.js
+++ src/Components/menu/SpringMenu.js
@@ -8,27 +8,27 @@ import lambGyro from './Lamb-Gyro.jpg';
 const SpringMenu = () => {
     return(
       <div className="container">
-        <div className="menu-page row">
+        <h1 className="text-center">SPRING MENU</h1>
 
         <div clasName="menu">
-          <ul className="menu-list col">
-            <li className="menu-item row-cols-1">Falafel Wrap..........$10</li>
-            <li className="menu-item row-cols-1">Chicken Tacos..........$8</li>
-            <li className="menu-item row-cols-1">Kale Salad..........$10</li>
-            <li className="menu-item row-cols-1">Lamb Gyro..........$11</li>
+          <ul className="menu-list text-center w-100">
+            <li className="menu-item row">Falafel Wrap..........$10</li>
+            <li className="menu-item row">Chicken Tacos..........$8</li>
+            <li className="menu-item row">Kale Salad..........$10</li>
+            <li className="menu-item row">Lamb Gyro..........$11</li>
           </ul>
           </div>
-        <div className="menu-pictures">
-          <ul className="img-list col">
-            <li className="menu-item-img"><img className="food-image -w" src={falafelWrap} alt="Falafel Wrap" /></li>
-            <li className="menu-item-img"><img className="food-image -w" src={chxTacos} alt="Chicken Tacos" /></li>
-            <li className="menu-item-img"><img className="food-image -w" src={kaleSalad} alt="Kale Salad" /></li>
-            <li className="menu-item-img"><img className="food-image -w" src={lambGyro} alt="Lamb Gyro" /></li>
+        <div className="menu-pictures w-100">
+          <ul className="img-list row w-100">
+            <li className="menu-item-img col"><img className="food-image -w" src={falafelWrap} alt="Falafel Wrap" /></li>
+            <li className="menu-item-img col"><img className="food-image -w" src={chxTacos} alt="Chicken Tacos" /></li>
+            <li className="menu-item-img col"><img className="food-image -w" src={kaleSalad} alt="Kale Salad" /></li>
+            <li className="menu-item-img col"><img className="food-image -w" src={lambGyro} alt="Lamb Gyro" /></li>
 
           </ul>
         </div>
         </div>
-        </div>
+      
   
 
 
