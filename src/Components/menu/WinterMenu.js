1aef95ac9de0fbe8982212fd21f1541a21b10697
Aubrena T <aubrenataylor@gmail.com>
2020-08-11 00:26:46 -0400

    completed winter menu

--- src/Components/menu/WinterMenu.js
+++ src/Components/menu/WinterMenu.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import './menu.css'
-import burger from '../../pictures/classicburger.jpg';
-import friedChicken from '../../pictures/friedChxSand.jpg';
-import healthyBowl from '../../pictures/healthyBowl.jpg';
-import smoothie from '../../pictures/smoothie.jpg';
+import corndog from '../../pictures/baconCornDog.jpg';
+import macAndCheese from '../../pictures/macAndCheese.jpg';
+import turkeySandwitch from '../../pictures/turkSand.jpg';
+import sliders from '../../pictures/sliders.jpg';
 
 const WinterMenu = () => {
   return (
     <div className="container">
-      <h1 className="text-center">WINTER Menu</h1>
+      <h1 className="text-center">WINTER MENU</h1>
 
       <div className="menu">
         <ul className="menu-list text-center w-100">
-          <li className="menu-item row">Classic Buger..........$10</li>
-          <li className="menu-item row">Crispy Chicken Sandwitch..........$12</li>
-          <li className="menu-item row">Quinoa Vegetable Bowl..........$9</li>
-          <li className="menu-item row">Strawberry Banana Smoothie..........$6</li>
+          <li className="menu-item row">Bacon Corndog..........$11</li>
+          <li className="menu-item row">Mac and Cheese..........$9</li>
+          <li className="menu-item row">Turkey Sandwitch..........$12</li>
+          <li className="menu-item row">Sliders(4)..........$18</li>
         </ul>
       </div>
       <div className="menu-pictures">
         <ul className="img-list row">
-          <li className="menu-item-img col"><img className="food-image -w" src={burger} alt="Classic Burger" /></li>
-          <li className="menu-item-img col"><img className="food-image -w" src={friedChicken} alt="Fried Chicken Sandwitch" /></li>
-          <li className="menu-item-img col"><img className="food-image -w" src={healthyBowl} alt="Veggie Bowl" /></li>
-          <li className="menu-item-img col"><img className="food-image -w" src={smoothie} alt="Smoothie" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={corndog} alt="Bacon Corndog" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={macAndCheese} alt="Macaroni and Cheese" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={turkeySandwitch} alt="Turkey Sandwitch" /></li>
+          <li className="menu-item-img col"><img className="food-image -w" src={sliders} alt="Hamburger sliders" /></li>
 
         </ul>
       </div>
