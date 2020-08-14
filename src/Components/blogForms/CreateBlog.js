75da7abf6fb1c046b8348aacf1a3e25ca3754920
Ethan Ferrier <Fethanerrier@gmail.com>
2020-08-14 14:39:34 -0700

    commited

--- src/Components/blogForms/CreateBlog.js
+++ src/Components/blogForms/CreateBlog.js
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-<<<<<<< HEAD
-import PropTypes from 'prop-types';
-import axios from 'axios';
-
-const CreateBlog = () => {
-=======
 import axios from 'axios';
 
 // json.parse turns the string into an object to return the token
@@ -15,7 +9,6 @@ const CreateBlog = () => {
     : false;
 
   //console.log(user);
->>>>>>> 3128eb83c5cffbc80517f1ecc6814a4d790ac908
   const [formData, setFormData] = useState({
     text: '',
     title: '',
@@ -24,7 +17,7 @@ const CreateBlog = () => {
   // text and title are the required fields from the backendroute. 
 
   //Get User token from Local Storage and set it to token
-  
+
   //log Create Blog then log the token to the console 
   console.log("Create Blog")
   console.log(token);
@@ -51,28 +44,6 @@ const CreateBlog = () => {
   const onSubmit = async e => {
     e.preventDefault();
     console.log(formData);
-<<<<<<< HEAD
-    const newBlog = {
-      title,
-      text,
-    };
-
-    try {
-      const config = {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      const body = JSON.stringify(newBlog);
-      const res = await axios.post(
-        'http://localhost:5000/api/posts',
-        body,
-        config
-      );
-      console.log(res.data);
-    } catch (err) {
-      console.error(err.res);
-=======
 
     if (!token) {
       //if there is NOT(!) a user
@@ -103,23 +74,15 @@ const CreateBlog = () => {
       } catch (err) {
         console.log(err);
       }
->>>>>>> 3128eb83c5cffbc80517f1ecc6814a4d790ac908
     }
   }
   //im axios and i make a post request to this localhost. body and config is the http request body.
 
   return (
-<<<<<<< HEAD
-    <div className='container col-9 col-md-7 mb-3'>
-      <div class='card row justify-content-center'>
-        <div class='card-header'>
-          <div className='h3 card-title'>Create a New Blog Form</div>
-=======
     <div className='container col-9 col-md-7 mb-3 shadow-lg  bg-white rounded'>
       <div className='card row justify-content-center'>
         <div className='card-header'>
           <div className='h3 card-title'>Create Blog Form</div>
->>>>>>> 3128eb83c5cffbc80517f1ecc6814a4d790ac908
           <div className='h6 card-subtitle mb-2 text-muted'>
             Create your next blog. make it Great!
           </div>
@@ -142,11 +105,8 @@ const CreateBlog = () => {
                 id='title'
                 className='form-control'
                 placeholder='Title'
-<<<<<<< HEAD
-=======
                 //for the onchange to work you must have the name and value properties
                 name='title'
->>>>>>> 3128eb83c5cffbc80517f1ecc6814a4d790ac908
                 value={title}
                 name='title'
                 onChange={e => onChange(e)}
