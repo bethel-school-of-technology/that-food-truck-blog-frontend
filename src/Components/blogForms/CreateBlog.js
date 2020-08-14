b853b0865bd55d08d7863b4a247f9d2a65639d7b
Val <valmbonilla@gmail.com>
2020-08-13 19:36:36 -0700

    reviewing createBlog

--- src/Components/blogForms/CreateBlog.js
+++ src/Components/blogForms/CreateBlog.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import axios from 'axios';
 
 const CreateBlog = () => {
@@ -7,10 +6,14 @@ const CreateBlog = () => {
     text: '',
     title: '',
   });
+// formData is an empty the state. setFormData is called and what changes the state to be - the NEW state.
+    // text and title are the required fields from the backendroute. 
 
+    // ask Ethan
   const token = JSON.parse(localStorage.getItem('jwtToken')) ? JSON.parse(localStorage.getItem('jwtToken')).token : false
   console.log(token);
 
+  // destructering formdata to keep clean and short code 
   const { title, text } = formData;
 
   const onChange = e =>
@@ -18,15 +21,24 @@ const CreateBlog = () => {
       ...formData,
       [e.target.name]: e.target.value,
     });
+    // onChange event changes the state. ... is a spread opperator that copies and pastes the state.
+    // e is an event that is being triggured and and we are targeting the name and value to assign an event 
   ///refers to the name attribute and value of the attribute being passed to it
 
   const onSubmit = async e => {
     e.preventDefault();
-    console.log(formData);
     const newBlog = {
       title,
       text,
     };
+        //e.preventDefault is a non cancelable event. when its clicked it is good to go. without this it would automatically submit.
+        //prevents this from happening until requested to run
+
+
+//the try has a const of config that holds header, token and content-type.
+//then const body stringifying newblog. taking it from plain text and making it JSON to submit to the backend.
+// const response. we have await axios -taking in the body and config. we are callin in the config an
+//passing in the config and the body.
 
     try {
       const config = {
@@ -46,6 +58,7 @@ const CreateBlog = () => {
       console.error(err.res);
     }
   }
+  //im axios and i make a post request to this localhost. body and config is the http request body.
 
   return (
     <div className='container col-9 col-md-7 mb-3'>
@@ -113,6 +126,5 @@ const CreateBlog = () => {
   );
 };
 
-CreateBlog.propTypes = {};
 
 export default CreateBlog;
